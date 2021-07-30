ba8ae82e966ce8336c01fecc52c19c148f1fae28
Jeongsu Yang <mycat83@gmail.com>
2021-07-30 23:08:07 +0900

    feat: split 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,10 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringTest {
+class StringTest {
+
     @Test
     void replace() {
+        // when
         String actual = "abc".replace("b", "d");
+
+        // then
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split1() {
+        // when
+        String[] values = "1,2".split(",");
+
+        // then
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        // when
+        String[] values = "1".split(",");
+
+        // then
+        assertThat(values).contains("1");
     }
 }
