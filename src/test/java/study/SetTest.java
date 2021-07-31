a9358252d08a1f455697a9eda4fed5ca19421d63
Jeongsu Yang <mycat83@gmail.com>
2021-07-31 22:48:55 +0900

    0단계 - 학습테스트 실습 (#1)
    * feat: split 테스트
    
    * feat: substring 테스트
    
    * feat: charAt 테스트
    
    * feat: size 테스트
    
    * feat: contains 테스트
    
    * feat: contains 테스트 @CsvSource 활용

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 메소드를 호출한다")
+    void size() {
+        // when
+        int size = numbers.size();
+
+        // then
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains 메소드를 호출한다")
+    void contains(int input, boolean expected) {
+        // given
+        boolean contains = numbers.contains(input);
+
+        // when
+        assertThat(contains).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,69 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class StringTest {
+class StringTest {
+
     @Test
+    @DisplayName("replace 메소드를 호출한다")
     void replace() {
+        // when
         String actual = "abc".replace("b", "d");
+
+        // then
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("split 메소드를 호출한다")
+    void split1() {
+        // when
+        String[] results = "1,2".split(",");
+
+        // then
+        assertThat(results).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("split 메소드를 단일 값으로 호출한다")
+    void split2() {
+        // when
+        String[] results = "1".split(",");
+
+        // then
+        assertThat(results).contains("1");
+    }
+
+    @Test
+    @DisplayName("substring 메소드를 호출한다")
+    void substring() {
+        // when
+        String result = "(1,2)".substring(1, 4);
+
+        // then
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메소드를 호출한다")
+    void charAt() {
+        // when
+        char result = "abc".charAt(1);
+
+        // then
+        assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt 메소드를 범위를 벗어난 인덱스로 호출하면 예외를 발생한다")
+    void charAtException() {
+        // when & then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(3))
+                .withMessageMatching("String index out of range: \\d+");
     }
 }
